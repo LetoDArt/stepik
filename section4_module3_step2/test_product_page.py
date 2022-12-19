9aa9b425a6bb397ee98c94a804fde974a685264b
Leto D-Art <goncharoff.k2018@yandex.ru>
2022-12-20 02:39:27 +0300

    solved task for section4 module2 step3

--- section4_module3_step2/test_product_page.py
+++ section4_module3_step2/test_product_page.py
@@ -2,7 +2,8 @@ import time
 from .pages.product_page import ProductPage
 
 
-link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+# link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019'
 
 def test_guest_can_go_to_login_page(browser):
     page = ProductPage(browser, link)
