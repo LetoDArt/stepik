a7c0b0316c35d46fb015f20d95a8332e30689ee3
Leto D-Art <goncharoff.k2018@yandex.ru>
2022-12-19 22:17:25 +0300

    section3_module6_step6

--- section3_module6_step6.py
+++ section3_module6_step6.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 
-# инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
-driver.get("https://stepik.org/lesson/25969/step/8")
+browser.get("https://stepik.org/lesson/25969/step/8")
